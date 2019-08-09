0bf309387c3073767b2de912b68be05b2c134e7a
kurtanr <robert.kurtanjek@gmail.com>
2019-08-09 09:09:08 +0200

    Added versioning to SimpleSudokuSolver.UI and updated version to 0.6.0.

--- SimpleSudokuSolver.UI/Properties/AssemblyInfo.cs
+++ SimpleSudokuSolver.UI/Properties/AssemblyInfo.cs
@@ -8,11 +8,15 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleSudokuSolver.UI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("UI for the SimpleSudokuSolver library")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+ [assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Robert Kurtanjek")]
 [assembly: AssemblyProduct("SimpleSudokuSolver.UI")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +55,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
+[assembly: AssemblyInformationalVersion("0.6.0.0")]
